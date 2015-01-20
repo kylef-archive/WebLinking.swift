1687c7d2fc93f997d7cf722c303dd29f6c219f9e
Kyle Fuller <inbox@kylefuller.co.uk>
2015-01-20 01:01:15 +0000

    Initial project

---
+++ WebLinking/WebLinking.h
@@ -0,0 +1,19 @@
+//
+//  WebLinking.h
+//  WebLinking
+//
+//  Created by Kyle Fuller on 20/01/2015.
+//  Copyright (c) 2015 Cocode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WebLinking.
+FOUNDATION_EXPORT double WebLinkingVersionNumber;
+
+//! Project version string for WebLinking.
+FOUNDATION_EXPORT const unsigned char WebLinkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebLinking/PublicHeader.h>
+
+
